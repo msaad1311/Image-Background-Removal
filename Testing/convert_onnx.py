1fcd43bbbfe776a04cbf1615b70a9715dab316ac
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2020-12-10 11:37:07 -0500

    carried out some failed tests

--- Testing/convert_onnx.py
+++ Testing/convert_onnx.py
@@ -7,14 +7,14 @@ import utils
 from torchvision import models
 from pytorch2keras.converter import pytorch_to_keras
 
-x = torch.randn(1,3,224, 224, requires_grad=False)
+x = torch.randn(1,3,1280, 1280, requires_grad=False)
 
 dlab = models.segmentation.deeplabv3_resnet101(pretrained=1).eval()
 
-# pytorch_to_keras(dlab, x, [(3,None, None,)], verbose=True, name_policy='short')
+k_model=pytorch_to_keras(dlab, x, [(3,None, None)], verbose=True, name_policy='short')
+k_model.save('model.h5')
+# input_names = [ "actual_input_1" ] + [ "learned_%d" % i for i in range(16) ]
+# output_names = [ "output1" ]
 
-input_names = [ "actual_input_1" ] + [ "learned_%d" % i for i in range(16) ]
-output_names = [ "output1" ]
-
-torch.onnx.export(dlab,x,'segmentation.onnx',verbose=True,input_names=input_names,
-                  output_names=output_names)
+# torch.onnx.export(dlab,x,'segmentation.onnx',verbose=True,input_names=input_names,
+#                   output_names=output_names)
