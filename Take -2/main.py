d3e0ee22521b5d91a21b19f4779dc1e423b0436a
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2021-01-18 13:17:47 -0500

    made some changes

--- Take -2/main.py
+++ Take -2/main.py
@@ -17,7 +17,7 @@ torch_transforms = transforms.Compose(
 )
 
 print('Load pre-trained MODNet...')
-pretrained_ckpt = './pretrained/modnet_webcam_portrait_matting.ckpt'
+pretrained_ckpt = r'Reference\pretrained\modnet_webcam_portrait_matting.ckpt'
 modnet = MODNet(backbone_pretrained=False)
 modnet = nn.DataParallel(modnet)
 
@@ -44,6 +44,8 @@ while(True):
     frame_np = cv2.resize(frame_np, (910, 512), cv2.INTER_AREA)
     frame_np = frame_np[:, 120:792, :]
     frame_np = cv2.flip(frame_np, 1)
+    bg = cv2.imread('background7.jpg')
+    bg = cv2.resize(bg,(frame_np.shape[1],frame_np.shape[0]))
 
     frame_PIL = Image.fromarray(frame_np)
     frame_tensor = torch_transforms(frame_PIL)
@@ -57,7 +59,7 @@ while(True):
     matte_tensor = matte_tensor.repeat(1, 3, 1, 1)
     matte_np = matte_tensor[0].data.cpu().numpy().transpose(1, 2, 0)
     fg_np = matte_np * frame_np + (1 - matte_np) * np.full(frame_np.shape, 255.0)
-    view_np = np.uint8(np.concatenate((frame_np, fg_np), axis=1))
+    view_np = np.uint8(np.concatenate((bg, fg_np), axis=1))
     view_np = cv2.cvtColor(view_np, cv2.COLOR_RGB2BGR)
 
     cv2.imshow('MODNet - WebCam [Press \'Q\' To Exit]', view_np)
